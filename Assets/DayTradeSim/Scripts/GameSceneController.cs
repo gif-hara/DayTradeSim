b63d26f6dd4f0a2985643aa4b9ec2955e7788875
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-11-11 18:05:50 +0900

    state

--- Assets/DayTradeSim/Scripts/GameSceneController.cs
+++ Assets/DayTradeSim/Scripts/GameSceneController.cs
@@ -1,12 +1,53 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using HK;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace DayTradeSim
 {
     public class GameSceneController : MonoBehaviour
     {
+        [SerializeField]
+        private string debugCommand;
+
+        private bool isGameEnd;
+
+        private TinyStateMachine stateMachine;
+        
         void Start()
         {
-            Debug.Log("GameSceneController Start");
+            BeginGameAsync(destroyCancellationToken).Forget();
+        }
+        
+        private async UniTask BeginGameAsync(CancellationToken scope)
+        {
+            stateMachine = new TinyStateMachine();
+            stateMachine.Change(PromptStateAsync);
+            await UniTask.WaitWhile(() => !isGameEnd, cancellationToken: scope);
+            stateMachine.Dispose();
+            Debug.Log("Game End");
+        }
+        
+        private async UniTask LoginStateAsync(CancellationToken scope)
+        {
+            Debug.Log("[State] Login Begin");
+            await UniTask.WaitWhile(() => !Keyboard.current.anyKey.wasPressedThisFrame, cancellationToken: scope);
+            Debug.Log("[State] Login End");
+            stateMachine.Change(PromptStateAsync);
+        }
+        
+        private async UniTask PromptStateAsync(CancellationToken scope)
+        {
+            Debug.Log("[State] Prompt Begin");
+            var result = await UniTask.WhenAny
+            (
+                UniTask.WaitWhile(() => !Keyboard.current.qKey.wasPressedThisFrame, cancellationToken: scope),
+                UniTask.WaitWhile(() => !Keyboard.current.enterKey.wasPressedThisFrame, cancellationToken: scope)
+            );
+            var prompt = result == 0 ? debugCommand : "TODO";
+            Debug.Log($"prompt = {prompt}");
+            Debug.Log("[State] Prompt End");
         }
     }
 }
