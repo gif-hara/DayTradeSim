1ba1933f8e742d28e4621a5fa16401f2316140e3
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-11-13 16:25:08 +0900

    rename

--- Assets/DayTradeSim/Scripts/Sequences/StockSimulationSystemUpdate.cs
+++ Assets/DayTradeSim/Scripts/Sequences/StockSimulationSystemUpdate.cs
@@ -1,21 +1,17 @@
 using System;
 using System.Threading;
 using Cysharp.Threading.Tasks;
-using UnityEngine;
 using UnitySequencerSystem;
 
 namespace DayTradeSim
 {
     [Serializable]
-    public class ViewStockSimulationSystem : ISequence
+    public class StockSimulationSystemUpdate : ISequence
     {
         public UniTask PlayAsync(Container container, CancellationToken cancellationToken)
         {
             var simulator = container.Resolve<StockSimulator.Core>();
-            foreach (var i in simulator.Companies)
-            {
-                Debug.Log($"{i.Name} : {i.StockPrice}");
-            }
+            simulator.Update();
             return UniTask.CompletedTask;
         }
     }
--- Assets/DayTradeSim/Scripts/Sequences/StockSimulationSystemView.cs
+++ Assets/DayTradeSim/Scripts/Sequences/StockSimulationSystemView.cs
@@ -1,17 +1,21 @@
 using System;
 using System.Threading;
 using Cysharp.Threading.Tasks;
+using UnityEngine;
 using UnitySequencerSystem;
 
 namespace DayTradeSim
 {
     [Serializable]
-    public class UpdateStockSimulationSystem : ISequence
+    public class StockSimulationSystemView : ISequence
     {
         public UniTask PlayAsync(Container container, CancellationToken cancellationToken)
         {
             var simulator = container.Resolve<StockSimulator.Core>();
-            simulator.Update();
+            foreach (var i in simulator.Companies)
+            {
+                Debug.Log($"{i.Id:0000} {i.Name} : {i.StockPrice}");
+            }
             return UniTask.CompletedTask;
         }
     }
