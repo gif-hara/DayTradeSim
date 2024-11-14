58d6006f79967a9cac1f3fd9b3c4aa5249e05325
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-11-14 12:17:05 +0900

    buy

--- Assets/DayTradeSim/Scripts/Sequences/StockSimulationSystemView.cs
+++ Assets/DayTradeSim/Scripts/Sequences/StockSimulationSystemView.cs
@@ -12,9 +12,15 @@ namespace DayTradeSim
         public UniTask PlayAsync(Container container, CancellationToken cancellationToken)
         {
             var simulator = container.Resolve<StockSimulator.Core>();
+            Debug.Log($"Money: {simulator.Money}");
             foreach (var i in simulator.Companies)
             {
-                Debug.Log($"{i.Id:0000} {i.Name} : {i.StockPrice}");
+                Debug.Log($"{i.Id:0000} {i.Name}: {i.StockPrice}");
+            }
+            foreach (var i in simulator.BuyList)
+            {
+                var company = simulator.GetCompany(i.Value);
+                Debug.Log($"Buy: {company.Name} {i.Value}");
             }
             return UniTask.CompletedTask;
         }
--- Assets/DayTradeSim/Scripts/StockSimulator/Core.cs
+++ Assets/DayTradeSim/Scripts/StockSimulator/Core.cs
@@ -7,12 +7,24 @@ namespace DayTradeSim.StockSimulator
         public List<Company> Companies { get; } = new();
         
         private int companyId = 1001;
+
+        public float Money { get; private set; }
+
+        public Dictionary<int, int> BuyList { get; } = new();
+
+        public enum BuyResult
+        {
+            Success,
+            NotEnoughMoney,
+            NotFoundCompany,
+        }
         
         public Core()
         {
             AddCompany("A", 100.0f);
             AddCompany("B", 200.0f);
             AddCompany("C", 300.0f);
+            Money = 1000000.0f;
         }
 
         public void Update()
@@ -23,9 +35,34 @@ namespace DayTradeSim.StockSimulator
             }
         }
         
+        public BuyResult Buy(int companyId, int quantity)
+        {
+            var company = Companies.Find(x => x.Id == companyId);
+            if (company == null)
+            {
+                return BuyResult.NotFoundCompany;
+            }
+            var price = company.StockPrice * quantity;
+            if (Money < price)
+            {
+                return BuyResult.NotEnoughMoney;
+            }
+            Money -= price;
+            if (!BuyList.TryAdd(companyId, quantity))
+            {
+                BuyList[companyId] += quantity;
+            }
+            return BuyResult.Success;
+        }
+        
         public void AddCompany(string name, float stockPrice)
         {
             Companies.Add(new Company(companyId++, name, stockPrice));
+        }
+        
+        public Company GetCompany(int companyId)
+        {
+            return Companies.Find(x => x.Id == companyId);
         }
     }
 }
