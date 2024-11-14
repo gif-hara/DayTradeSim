4ede3d67a77537b9e3b216cd4abe15f2bdfdaf78
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-11-14 16:38:55 +0900

    Define

---
+++ Assets/DayTradeSim/Scripts/Define.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+
+namespace DayTradeSim
+{
+    public static class Define
+    {
+        public enum CompanyCategory
+        {
+            /// <summary>
+            /// 情報技術
+            /// </summary>
+            InformationTechnology,
+
+            /// <summary>
+            /// 金融
+            /// </summary>
+            Finance,
+
+            /// <summary>
+            /// 小売
+            /// </summary>
+            Retail,
+
+            /// <summary>
+            /// エンターテインメント
+            /// </summary>
+            Entertainment,
+
+            /// <summary>
+            /// 食品
+            /// </summary>
+            Food,
+
+            /// <summary>
+            /// 医療
+            /// </summary>
+            Medical,
+
+            /// <summary>
+            /// 化学
+            /// </summary>
+            Chemical,
+
+            /// <summary>
+            /// エネルギー
+            /// </summary>
+            Energy,
+
+            /// <summary>
+            /// 建設
+            /// </summary>
+            Construction,
+
+            /// <summary>
+            /// 輸送
+            /// </summary>
+            Transportation,
+
+            /// <summary>
+            /// 通信
+            /// </summary>
+            Communication,
+
+            /// <summary>
+            /// 教育
+            /// </summary>
+            Education,
+
+            /// <summary>
+            /// 不動産
+            /// </summary>
+            RealEstate,
+
+            /// <summary>
+            /// 農業
+            /// </summary>
+            Agriculture,
+
+            /// <summary>
+            /// 鉱業
+            /// </summary>
+            Mining,
+
+            /// <summary>
+            /// ゲーム開発
+            /// </summary>
+            GameDevelopment,
+        }
+    }
+}
